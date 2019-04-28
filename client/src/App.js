d46635e256a03ae7211844d2fb36c54fb58e6b42
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-04-27 17:28:57 -0700

    Set up basic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div>
-      <h1>Choose For Me</h1>
-    </div>
-  );
-}
+import Home from './pages/Home'
+import Choose from './pages/Choose'
 
-export default App
+export default () => (
+  <Router>
+    <Route path='/' exact component={Home} />
+    <Route path='/choose' component={Choose} />
+  </Router>
+)
---
+++ client/src/pages/Choose.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>Choose</h1>
+  </div>
+)
